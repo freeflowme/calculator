5c0b2c9064e32d682214b6f7816a47adf9d8f0ef
freeflowme <116676605+freeflowme@users.noreply.github.com>
2022-12-30 16:05:05 -0600

    Add four basic calculator functions (+ _ * /)

---
+++ script.js
@@ -0,0 +1,13 @@
+//basic calculator functions
+const add = function(a, b) {
+    return a + b; 
+};
+const subtract = function(c, d) {
+    return c - d;
+};
+const mutiply = function(e, f) {
+    return e * f;
+};
+const divide = function(g, h) {
+    return g / h; 
+};
