cdf9ed3b470a91e7ff7036a6f53c6e7155ecd1cc
freeflowme <116676605+freeflowme@users.noreply.github.com>
2022-12-31 16:26:09 -0600

    Add button eventListener("click") functionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+/*
 let input1 = prompt('Please enter a number');
 let x = parseFloat(input1);
 let operator = prompt("Please enter +, -, *, or /");
 let input2 = prompt('Please enter a number');
 let y = parseFloat(input2);
+*/
 
 //basic calculator functions
 function add(x, y) {
@@ -20,17 +22,27 @@ function divide(x, y) {
 
 //call coresponding basic function 
 function operate(x, operator, y) {
-    if(typeof x !== 'number' || typeof y !== 'number') {
+    if(typeof x !== "number" || typeof y !== "number") {
         return
-    } else if(operator === '+') {
+    } else if(operator === "+") {
         return add(x, y);
-    } else if(operator === '-') {
+    } else if(operator === "-") {
         return subtract(x, y);
-    } else if(operator === '*') {
+    } else if(operator === "*") {
         return multiply(x, y);
-    } else if(operator === '/') {
+    } else if(operator === "/") {
         return divide(x, y);
     }
 };
 
+/*
 console.log(operate(x, operator, y));
+*/
+
+//button functionality
+const numberBtns = document.querySelectorAll(".numberBtn");
+    numberBtns.forEach(numberBtn => {
+    numberBtn.addEventListener("click", () => {
+    console.log(numberBtn.value);
+    });
+});
