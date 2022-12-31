9187139a5a5aa8c79edf86e185aa4c2e3e595a53
freeflowme <116676605+freeflowme@users.noreply.github.com>
2022-12-30 20:25:46 -0600

    Add operate() and user prompts

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+let input1 = prompt('Please enter a number');
+let x = parseFloat(input1);
+let operator = prompt("Please enter +, -, *, or /");
+let input2 = prompt('Please enter a number');
+let y = parseFloat(input2);
+
 //basic calculator functions
-const add = function(a, b) {
-    return a + b; 
+function add(x, y) {
+    return x + y; 
 };
-const subtract = function(c, d) {
-    return c - d;
+function subtract(x, y) {
+    return x - y;
 };
-const mutiply = function(e, f) {
-    return e * f;
+function multiply(x, y) {
+    return x * y;
 };
-const divide = function(g, h) {
-    return g / h; 
+function divide(x, y) {
+    return x / y; 
 };
+
+//call coresponding basic function 
+function operate(x, operator, y) {
+    if(typeof x !== 'number' || typeof y !== 'number') {
+        return
+    } else if(operator === '+') {
+        return add(x, y);
+    } else if(operator === '-') {
+        return subtract(x, y);
+    } else if(operator === '*') {
+        return multiply(x, y);
+    } else if(operator === '/') {
+        return divide(x, y);
+    }
+};
+
+console.log(operate(x, operator, y));
